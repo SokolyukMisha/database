de3ee9aab751cee0f1ac20536c7c559dcb1c8307
SokolyukMisha <70840330+SokolyukMisha@users.noreply.github.com>
2021-06-03 09:58:19 +0300

    Update test.js

--- test.js
+++ test.js
@@ -21,3 +21,30 @@ console.log(TB);
 //change item
 TB.change(1, 'name', 'Ivan');
 console.log(TB);
+//search
+console.log(TB.searchByKey('name'));
+console.log(TB.searchByValue('Sasha'));
+console.log(TB.searchByValue({ name: 'Misha', number: +380985660644 }));
+
+//remowe
+TB.remove(3);
+console.log(TB);
+
+//sort
+TB.sort('name', 1);
+console.log(TB);
+
+
+//concatenation
+const table = new HashTable();
+
+table.addMany([
+  { name: 'Tolik', Number: +380986233900, age: 19 },
+  { name: 'Danya', Number: +380664346307, age: 18 },
+  { name: 'Misha', Number: +380985660644, age: 17 },
+  { name: 'Sasha', Number: +380955396374, age: 18 },
+  { name: 'Vanya', Number: +380955396374, age: 18 },
+]);
+
+TB.concatenation(table);
+console.log(TB);
